2e844565adb06fef9f5b4a4ab66aba7cd3423227
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-06-10 03:30:21 +0000

    todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import "../styles/home.css";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setinputValue] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  const handleAdd = (e) => {
+    if (
+      (e.key === "Enter" && inputValue.trim() !== "") ||
+      (e.type === "click" && inputValue.trim() !== "")
+    ) {
+      const newTask = [...todo, inputValue];
+      setTodo(newTask);
+      setinputValue("");
+    }
+  };
+
+  const handleDelete = (i) => {
+    const newTask = todo.filter((task, index) => index !== i);
+    setTodo(newTask);
+  };
+
+  return (
+    <div className="main text-center">
+      <h1>ToDo List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setinputValue(e.target.value)}
+        onKeyUp={handleAdd}
+      />
+      <button onClick={handleAdd}>Add</button>
+      <ul>
+        {todo.length === 0 ? (
+          <li>No tasks. Add tasks, lazy!</li>
+        ) : (
+          todo.map((task, index) => (
+            <li key={index}>
+              {task}{" "}
+              <button onClick={() => handleDelete(index)}>
+                {" "}
+                {/* <FontAwesomeIcon icon={faTrash} />{" "} */}
+              </button>
+            </li>
+          ))
+        )}
+        <span>{todo.length} tasks</span>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
